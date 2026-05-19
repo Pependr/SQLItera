4958764b9baefb532a1717f38453a990edd2f848
Pependr <antonkudrikov88@gmail.com>
2026-05-19 21:41:56 +0300

    Tests for operations

--- src/sqlitera/operations.py
+++ src/sqlitera/operations.py
@@ -3,7 +3,6 @@ import contextlib as cl
 from typing import Any, Callable, Iterable, Protocol, Generator
 
 from sqlitera.protocols import Cursor, Aggregate, Connection
-from sqlitera.schemes.columns import filter_by_columns
 
 
 @cl.contextmanager
@@ -28,15 +27,10 @@ def create(conn: Connection, scheme: CreateScheme) -> None:
 class InsertScheme(Protocol):
 	def insert(self, inputs: Iterable[str]) -> str: ...
 
-	@property
-	def columns(self) -> Iterable[Any]: ...
 
-
-def insert(conn: Connection, scheme: InsertScheme, **items: Any) -> None:
-	inputs: dict[str, Any] = filter_by_columns(items, scheme.columns)
-
+def insert(conn: Connection, scheme: InsertScheme, **inputs: Any) -> None:
 	with cursor(conn) as cur:
-		cur.execute(scheme.insert(inputs), tuple(inputs.values()))
+		cur.execute(scheme.insert(inputs.keys()), tuple(inputs.values()))
 
 
 type AggregateFn[I, O] = Callable[[Iterable[Iterable[I]]], O]
@@ -45,18 +39,13 @@ type AggregateFn[I, O] = Callable[[Iterable[Iterable[I]]], O]
 def aggregate[I, O](aggr_fn: AggregateFn[I, O]) -> type[Aggregate[I, O]]:
 	class AggrCls:
 		def __init__(self) -> None:
-			self.buffer: list[list[I]] = []
+			self.buffer: list[tuple[I, ...]] = []
 
-		# @classmethod
-		# def __call__(cls, cols: Iterable[Iterable[I]]) -> O:
-		# 	return aggr_fn(cols)
+		def __call__(self, cols: Iterable[Iterable[I]]) -> O:
+			return aggr_fn(cols)
 
 		def step(self, *values: I) -> None:
-			while len(values) > len(self.buffer):
-				self.buffer.append([])
-
-			for val, col in zip(values, self.buffer):
-				col.append(val)
+			self.buffer.append(values)
 
 		def finalize(self) -> O:
 			return aggr_fn(self.buffer)
--- tests/sqlitera/test_operations.py
+++ tests/sqlitera/test_operations.py
@@ -3,6 +3,8 @@ import pytest_mock as pm
 
 import contextlib as cl
 
+from typing import Iterable, cast
+
 import sqlitera.protocols as pts
 import sqlitera.operations as ops
 
@@ -17,23 +19,85 @@ def conn(mocker: pm.MockerFixture) -> pm.MockType:
 	return mocker.Mock(spec_set=pts.Connection)
 
 
-def test_cursor(conn: pm.MockType, cur: pm.MockType) -> None:
+def test_cursor(
+	conn: pm.MockType, cur: pm.MockType, subtests: pt.Subtests
+) -> None:
 	conn.cursor.return_value = cur
 
-	with ops.cursor(conn):
-		...
+	with subtests.test("Error on cursor() call"):
+		with ops.cursor(conn):
+			...
 
-	conn.cursor.assert_called_once()
-	cur.close.assert_called_once()
+		conn.cursor.assert_called_once()
+		cur.close.assert_called_once()
 
 	conn.reset_mock()
 	cur.reset_mock()
 
 	class OopsieDaisy(RuntimeError): ...
 
-	with cl.suppress(OopsieDaisy):
-		with ops.cursor(conn):
-			raise OopsieDaisy
+	with subtests.test("Error on cursor() call with exception"):
+		with cl.suppress(OopsieDaisy):
+			with ops.cursor(conn):
+				raise OopsieDaisy
 
-	conn.cursor.assert_called_once()
-	cur.close.assert_called_once()
+		conn.cursor.assert_called_once()
+		cur.close.assert_called_once()
+
+
+def test_create(
+	mocker: pm.MockerFixture, conn: pm.MockType, cur: pm.MockType
+) -> None:
+	conn.cursor.return_value = cur
+
+	mock_scheme = mocker.Mock(spec_set=ops.CreateScheme)
+	mock_scheme.create_query = "CREATE MOCK"
+
+	ops.create(conn, mock_scheme)
+
+	cur.execute.assert_called_once_with("CREATE MOCK")
+
+
+def test_insert(
+	mocker: pm.MockerFixture, conn: pm.MockType, cur: pm.MockType
+) -> None:
+	conn.cursor.return_value = cur
+
+	mock_scheme = mocker.Mock(spec_set=ops.InsertScheme)
+	mock_scheme.insert.return_value = "INSERT INTO MOCK"
+
+	data: dict[str, int] = {f"mock{i}": i for i in range(1, 4)}
+
+	ops.insert(conn, mock_scheme, **data)
+
+	mock_scheme.insert.assert_called_once_with(data.keys())
+	cur.execute.assert_called_once_with("INSERT INTO MOCK", (1, 2, 3))
+
+
+def test_aggregate(mocker: pm.MockerFixture, subtests: pt.Subtests) -> None:
+	def my_aggregate(cols: Iterable[Iterable[int]]) -> int:
+		return sum(sum(row) for row in cols)
+
+	mock_aggr_fn = mocker.Mock(wraps=my_aggregate)
+
+	aggr_obj = cast(pts.Aggregate[int, int], ops.aggregate(mock_aggr_fn)())
+
+	sample: list[list[int]] = [
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 9],
+	]
+
+	with subtests.test("Error on calling aggregate object"):
+		assert aggr_obj(sample) == my_aggregate(sample)
+		mock_aggr_fn.assert_called_once_with(sample)
+
+	mock_aggr_fn.reset_mock()
+
+	with subtests.test("Error on using aggregate object"):
+		for row in sample:
+			aggr_obj.step(*row)
+
+		assert aggr_obj.finalize() == my_aggregate(sample)
+
+		mock_aggr_fn.assert_called_once_with([(1, 2, 3), (4, 5, 6), (7, 8, 9)])
